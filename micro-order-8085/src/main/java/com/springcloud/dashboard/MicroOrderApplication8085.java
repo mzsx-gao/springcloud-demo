a7d0635454884879de6863a97fb3dc5a6bea3328
gaoshudian <18818196720@163.com>
2020-05-20 22:02:58 +0800

    dd

--- micro-order-8085/src/main/java/com/springcloud/dashboard/MicroOrderApplication8085.java
+++ micro-order-8085/src/main/java/com/springcloud/dashboard/MicroOrderApplication8085.java
@@ -7,8 +7,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 //开启eureka客户端功能
 @EnableEurekaClient
-public class MicroOrderApplication {
+public class MicroOrderApplication8085 {
     public static void main(String[] args) {
-        SpringApplication.run(MicroOrderApplication.class,args);
+        SpringApplication.run(MicroOrderApplication8085.class,args);
     }
 }
